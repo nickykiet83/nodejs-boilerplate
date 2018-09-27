b69dfe8357f1729d0f418d50955d36fc80db5b7a
Kiet Pham <phamquanganhkiet@gmail.com>
2018-09-27 14:19:22 +1000

    init project

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var secret = require('./config/secret');
+var engine = require('ejs-mate');
+var app = express();
+
+//middleware
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+//register routes
+var mainRoutes = require('./routes/main');
+
+
+app.use(mainRoutes);
+
+app.listen(secret.port, (err) => {
+  if (err) throw err;
+  console.log(`Server is Runnig at port: ${secret.port}`);
+})
+
